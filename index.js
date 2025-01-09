28bbe165742cfbe7279026c23ceb382cace0dade
James Gary <gmarchesi888@gmail.com>
2025-01-09 17:48:20 -0500

    WHY IS THIS NOT WORKING?

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const pool = require('./db')
 
 const app = express()
 const PORT = 3000 || process.env.PORT
@@ -12,7 +13,16 @@ app.get('/', (req, res) => {
     res.send('Hello From The Server!')
 })
 
+app.get('/events', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM events')
+        res.status(200).json(result.rows)
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({ error: 'Database Error' })
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`)
 })
-
