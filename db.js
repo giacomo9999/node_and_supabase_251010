fe5f1da8e68fdc694683f9952f302c84565b323b
James Gary <gmarchesi888@gmail.com>
2025-01-16 14:27:07 -0500

    Still not working.

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 1
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL_TEST,
     ssl: { rejectUnathorized: false },
